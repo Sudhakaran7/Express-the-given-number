33a2a4adf8f53d884c3ac3cf23200c90636a80c1
Sudhakaran_roni <32321500+Sudhakaran7@users.noreply.github.com>
2020-08-10 07:25:41 +0530

    Create expressnumber.py

---
+++ expressnumber.py
@@ -0,0 +1,22 @@
+from functools import lru_cache
+
+class Solution(object):
+    def leastOpsExpressTarget(self, x, target):
+        cost = list(range(40))
+        cost[0] = 2
+
+        @lru_cache(None)
+        def dp(i, targ):
+            if targ == 0: return 0
+            if targ == 1: return cost[i]
+            if i >= 39: return float('inf')
+
+            t, r = divmod(targ, x)
+            return min(r * cost[i] + dp(i+1, t),
+                       (x-r) * cost[i] + dp(i+1, t+1))
+
+        return dp(0, target) - 1
+
+val=Solution()
+n,target=map(int,input().split())
+print(val.leastOpsExpressTarget(n,target))
